cfa40704ea9d0aa6c2717a7361fcfd5613cfe12a
wearde <wearde.studio@gmail.com>
2015-07-02 00:05:52 +0300

    init comit

---
+++ Component.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * @author WeArDe <wearde.studio@gmail.com>
+ * @link http://wearde.pp.ua
+ */
+
+namespace amass\langprettyurl;
+
+
+use \lajax\translatemanager\models\Language;
+use yii\base\UnknownClassException;
+
+/**
+ * Component
+ * helper to form an array of languages, working together with yii2-translate-manager https://github.com/lajax/yii2-translate-манагер
+ * You can use a class dear
+ * 'component' => [
+ *  .....
+ *     'urlManager' => [
+ *        'class' => 'amass\langprettyurl\UrlManager',
+ *        'allowClass' => 'amass\langprettyurl\Component',
+ *     ],
+ *  .....
+ * ]
+ */
+
+class Component extends \yii\base\Component{
+
+    /** $var $languages array*/
+    public $languages;
+
+    /**
+     * @inheritdoc
+     */
+    public function init(){
+
+        if(is_null($this->languages ))
+            $this->languages = $this->_loadLanguages();
+    }
+
+    /**
+     * @throw yii\base\UnknownClassException
+     * @return mixed
+     */
+    private function _loadLanguages(){
+        $array = [];
+        if(!class_exists(\lajax\translatemanager\models\Language::className()))
+            throw new UnknownClassException(\lajax\translatemanager\models\Language::className().' don\'t exists check yii2-translate-manager https://github.com/lajax/yii2-translate-manager');
+
+        $model = Language::getLanguages(true, true);
+        foreach($model as $language){
+            $array[$language['language']] = $language['language_id'];
+        }
+        return $array;
+    }
+}
---
+++ README.md
@@ -0,0 +1,31 @@
+Yii2 Language Pretty Local Url
+==============================
+Yii2 Language Pretty Local Url
+
+Installation
+------------
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```
+php composer.phar require --prefer-dist amass/local-language-url "*"
+```
+
+or add
+
+```
+"amass/local-language-url": "*"
+```
+
+to the require section of your `composer.json` file.
+
+
+Usage
+-----
+
+Once the extension is installed, simply use it in your code by  :
+
+```php
+<?= \amass\langprettyurl\AutoloadExample::widget(); ?>```
